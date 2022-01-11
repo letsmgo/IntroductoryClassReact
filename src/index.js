b8b01e3977a92f776917f370b5bd92510ee70521
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-10 23:32:41 -0300

    Use the component created in index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FirstComponent from './components/FirstComponent';
 import './index.css';
 
 ReactDOM.render(
-<h1>Hello World!</h1>,
-  document.getElementById('root')
+  <div>
+    <FirstComponent/>
+  </div>
+
+,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+ 
