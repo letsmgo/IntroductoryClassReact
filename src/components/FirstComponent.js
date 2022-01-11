78edd7bf967d2fffc7f859c9ab6586300f738e1d
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-10 23:30:34 -0300

    implement first component in Js

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+function FirstComponent(){
+    return (
+        <div>
+            <h1> Hello World! </h1>
+            <h2>Welcome to the class of react</h2>
+        </div>
+       
+    )
+}
+
+export default FirstComponent;
