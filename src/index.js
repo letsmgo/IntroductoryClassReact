1becef731ead349838a9566f2895e9bfe49ffdc6
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-11 00:33:08 -0300

    save all changes to index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FirstComponent from './components/FirstComponent';
-import ComponentWithProps from './components/ComponentWithProps';
+import Card from './components/layouts/Cards';
 import './index.css';
 
 ReactDOM.render(
   <div>
-    <ComponentWithProps nome="André" cargo="Instrutor"/>
-    <ComponentWithProps nome="Vini" cargo="Instrutor"/>
-    <ComponentWithProps nome="Maga" cargo="GP"/>
-    <ComponentWithProps nome="Gi" cargo="GP"/>
+    <Card title="Card #1" contents="Class of this card"></Card>
   </div>
 
 ,document.getElementById('root')
